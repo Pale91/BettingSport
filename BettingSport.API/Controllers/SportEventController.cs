9aa17673bb47611afa265453b4c62385ea8402c5
Alejandro Palmer (c) <palejandro@vmware.com>
2020-09-17 23:19:35 +0300

    Finishing API. Pending validations

--- BettingSport.API/Controllers/SportEventController.cs
+++ BettingSport.API/Controllers/SportEventController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BettingSport.API.Controllers
 {
+    // https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-3.1&tabs=visual-studio
     [Route("api/[controller]")]
     [ApiController]
     public class SportEventController : ControllerBase
@@ -38,6 +39,10 @@ namespace BettingSport.API.Controllers
         public async Task<ActionResult<SportEvent>> Get(int id)
         {
             var _event = await readonlyRepository.GetAsync(id);
+            if(_event == null)
+            {
+                return NotFound();
+            }
             return Ok(_event);
         }
 
@@ -49,6 +54,33 @@ namespace BettingSport.API.Controllers
             await unitOfWork.CommitChangesAsync();
 
             return CreatedAtAction(nameof(Get), new { id = sportEvent.Id }, sportEvent);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<SportEvent>> Update(int id, SportEvent sportEvent)
+        {
+            if (id != sportEvent.Id)
+                return BadRequest();
+
+            repository.Update(sportEvent);
+            await unitOfWork.CommitChangesAsync();
+
+            // Choosing OK (200) over NoConten (204) in order to retrieve the entity in the response 
+            return Ok(sportEvent);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<SportEvent>> Delete(int id)
+        {
+            var _event = await readonlyRepository.GetAsync(id);
+
+            if(_event == null)
+            {
+                return NotFound();
+            }
+            repository.Delete(_event);
+            await unitOfWork.CommitChangesAsync();
+            return NoContent();
         }
 
     }
