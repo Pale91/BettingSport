df6f1829eaed187bfe5767d548741de5e1ac05ba
Alejandro Palmer (c) <palejandro@vmware.com>
2020-09-19 20:24:35 +0300

    Adding swagger

--- BettingSport.API/Startup.cs
+++ BettingSport.API/Startup.cs
@@ -36,6 +36,7 @@ namespace BettingSport.API
 
             // Adding service
             services.AddEf(Configuration);
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,15 @@ namespace BettingSport.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = String.Empty;
+            });
 
             app.UseRouting();
 
